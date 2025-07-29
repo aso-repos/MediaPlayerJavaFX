f69a2fe3bef220bece6e1f816c880d122464558d
Wessel <wessel@autumnskies-online.co.za>
2025-07-30 00:47:39 +0200

    Main method ammended

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main (String[] args) {
 
-        System.out.println("This is a test for now.");
+        System.out.println("This is a test right now.");
 
     }
 }
