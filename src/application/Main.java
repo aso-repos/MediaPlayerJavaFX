61b5b08ef33e4d1cbfdd4b51478fb82cefb8c2f0
Wessel <wessel@autumnskies-online.co.za>
2025-07-30 21:38:08 +0200

    FXML File added and Main ammended

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,30 @@
 package application;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start (Stage stage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("/mp3player.fxml"));
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+
+
+
+    }
+
 
     public static void main (String[] args) {
 
-        System.out.println("This is a test right now.");
-
+        launch(args);
+        }
     }
 }
